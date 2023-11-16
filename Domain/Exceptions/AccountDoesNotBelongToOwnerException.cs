198a66bb7ffaa06ef12be01f716a06cf927ce89a
iparkuSS <iparkuSS@mail.ru>
2023-11-16 20:12:58 +0300

    добавил уровень домена, сервисов

---
+++ Domain/Exceptions/AccountDoesNotBelongToOwnerException.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Exceptions.Base;
+
+namespace Domain.Exceptions
+{
+    public sealed class AccountDoesNotBelongToOwnerException : BadRequestException
+    {
+        public AccountDoesNotBelongToOwnerException(Guid ownerId, Guid accountId)
+            : base($"The account with the identifier {accountId} does not belong to the owner with the identifier {ownerId}") { }
+    }
+}
---
+++ Domain/Exceptions/OwnerNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Exceptions.Base;
+
+namespace Domain.Exceptions
+{
+    public sealed class OwnerNotFoundException : NotFoundException
+    {
+        public OwnerNotFoundException(Guid ownerId)
+            : base($"The owner with the identifier {ownerId} was not found.") { }
+    }
+}
